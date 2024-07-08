bae88a1ec093d3bb7113a9f7c3764cd177154065
quanluncodedao <tranminhquan2704062003@gmail.com>
2024-07-08 09:22:36 +0700

    Doctor details

--- Frontend/src/store/reducers/adminReducer.js
+++ Frontend/src/store/reducers/adminReducer.js
@@ -7,6 +7,7 @@ const initialState = {
     positions: [],
     users: [],
     topDoctors: [],
+    allDoctors: [],
 }
 
 const adminReducer = (state = initialState, action) => {
@@ -75,14 +76,27 @@ const adminReducer = (state = initialState, action) => {
             state.topDoctors = action.dataDoctors;
             return {
                 ...state,
-            }
+        }
         
         case actionTypes.FETCH_TOP_DOCTORS_FAILED:
             state.topDoctors = [];
             return {
                 ...state,
-            }
-
+        }
+        
+        case actionTypes.FETCH_ALL_DOCTORS_SUCCESS:
+            state.allDoctors = action.dataDr;
+      
+            return {
+              ...state,
+            };
+      
+          case actionTypes.FETCH_ALL_DOCTORS_FAILED:
+            state.allDoctors = [];
+      
+            return {
+              ...state,
+            };
         default:
             return state;
     }
